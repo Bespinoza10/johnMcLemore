2052957c50047943563d4f2bec3d08aba1e7846a
Brandon Espinoza <espinozab100@gmail.com>
2016-05-17 11:02:40 -0500

    CRUD Ability for creating posts is completed.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    
+
   end
 
   def new
@@ -18,10 +19,35 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @post.update post_params
+      redirect_to @post, notice: "Great! Your Article was successfully saved!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 
 end
