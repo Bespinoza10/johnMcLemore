3c1cb9e975f96ae7270c2a76e3673d494e6de28d
Brandon Espinoza <espinozab100@gmail.com>
2016-05-20 17:19:12 -0500

    looping recent 6 posts on homepage and added text to about and work section

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    # @post = Post.all.order("created_at desc")
+    @posts = Post.all.limit(6).order("created_at desc")
   end
 
 end
