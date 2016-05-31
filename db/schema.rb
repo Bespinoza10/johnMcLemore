2e80fadbb07140f2eee7d21a48c5e8b057a3e66b
Brandon Espinoza <espinozab100@gmail.com>
2016-05-31 10:05:39 -0500

    Added date to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523154645) do
+ActiveRecord::Schema.define(version: 20160531150200) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160523154645) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "slug"
+    t.string   "date"
   end
 
   add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true
